78913dd72c89b1d9e73fd9bb96767848cd208423
yichunzhao <zhaoyichun@hotmail.com>
2020-07-07 19:19:50 +0200

    Initial commit

---
+++ src/main/java/com/ynz/haldemo/HaldemoApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.haldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynz/haldemo/HaldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.haldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
