a46b74502fcb99f988b251d7e17fe933fc802a06
yichunzhao <zhaoyichun@hotmail.com>
2020-07-08 11:20:32 +0200

    solving repository ID error type

--- src/main/java/com/ynz/haldemo/repository/AuthorRepository.java
+++ src/main/java/com/ynz/haldemo/repository/AuthorRepository.java
@@ -4,6 +4,8 @@ import com.ynz.haldemo.domain.Author;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
-public interface AuthorRepository extends PagingAndSortingRepository<Author, Integer> {
+public interface AuthorRepository extends PagingAndSortingRepository<Author, UUID> {
 }
--- src/main/java/com/ynz/haldemo/repository/BookRepository.java
+++ src/main/java/com/ynz/haldemo/repository/BookRepository.java
@@ -4,6 +4,8 @@ import com.ynz.haldemo.domain.Book;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
-public interface BookRepository extends PagingAndSortingRepository<Book, Integer> {
+public interface BookRepository extends PagingAndSortingRepository<Book, UUID> {
 }
